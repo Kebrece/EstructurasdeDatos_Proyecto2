cbb2cc16bd5c898d625866e558f28c5b883a8b08
Kebrece <72960066+Kebrece@users.noreply.github.com>
2021-11-28 22:19:17 -0600

    Referencias agregadas

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ parser.imprimirArchivo()
 table = hashTable.HashTable()
 table.readCode(parser.getFileName())
 
+#Referencias:
+#Singh, Ravendra, Vivek Sharma, and Manish Varshney. Design and Implementation of Compiler. Daryaganj: New Age International, 2009. Web.
+#Capítulo 5  (Este recurso lo consiguen en el catálogo del SIDUNA)
 
+#Drozdek, A. (2013). Data Structures and algorithms in C++. Cengage Learning.
+#Sección 12.5: Caso de Estudio (Dentro del folder de libros compartidos)
